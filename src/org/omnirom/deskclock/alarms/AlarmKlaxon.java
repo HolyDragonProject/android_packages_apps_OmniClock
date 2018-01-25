cc55eb0cc267efaa8b1140692e5d7888f11e768f
maxwen <max.weninger@gmail.com>
2018-01-25 15:38:52 +0100

    OmniClock: stop loophole and make sure no alarm sound continue to play
    Change-Id: Ic8b5784be1a026ac0f97bc2494ff0e6f84961528

--- src/org/omnirom/deskclock/alarms/AlarmKlaxon.java
+++ src/org/omnirom/deskclock/alarms/AlarmKlaxon.java
@@ -159,6 +159,7 @@ public class AlarmKlaxon {
 
     public static void start(final Context context, AlarmInstance instance) {
         sContext = context;
+        sStarted = true;
 
         // Make sure we are stop before starting
         stop(context);
@@ -288,7 +289,6 @@ public class AlarmKlaxon {
             Vibrator vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
             vibrator.vibrate(sVibratePattern, 0);
         }
-        sStarted = true;
 
         if (sStreamMediaMode) {
             IntentFilter intentFilter = new IntentFilter();
@@ -301,6 +301,9 @@ public class AlarmKlaxon {
         LogUtils.v("playAlarm");
         if (sMediaPlayer != null) {
             sMediaPlayer.reset();
+        }
+        if (!sStarted) {
+            return;
         }
         sMediaPlayer = new MediaPlayer();
         sMediaPlayer.setOnErrorListener(new OnErrorListener() {
@@ -363,6 +366,9 @@ public class AlarmKlaxon {
 
     // Do the common stuff when starting the alarm.
     private static void startAlarm(final Context context, MediaPlayer player) throws IOException {
+        if (!sStarted) {
+            return;
+        }
         LogUtils.v("startAlarm");
 
         LogUtils.v("Using audio stream " + (getAudioStream(context) == AudioManager.STREAM_MUSIC ? "Music" : "Alarm"));
